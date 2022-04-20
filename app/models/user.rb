2deff79eef79a2e21f8de8c91d07d33a4f171f46
KAMEchan01 <shinichi.kameda001@gmail.com>
2022-04-20 21:48:23 +0900

    テストコードの成功

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,21 @@ class User < ApplicationRecord
   
   has_many :items
   has_many :orders
+
+  with_options presence: true do
+    # 存在すること・確認用を含めて2回入力・6字以上はdeviseのデフォルト実装のため省略
+    # 半角英数字（空文字NG）以外の場合には、メッセージを出す
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+
+    validates :nickname
+    # @含むこと・存在することはdeviseのデフォルト実装のため省
+    # 全角ひらがな、全角カタカナ、漢字
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    # 全角カタカナ
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birth_date
+  end
 end
