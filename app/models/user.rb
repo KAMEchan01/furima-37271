dfda99cc955b0d3af7c06528cabd4b4a85e6f1e9
KAMEchan01 <shinichi.kameda001@gmail.com>
2022-04-21 23:31:59 +0900

    全角ひらがな、全角カタカナ、漢字（鬼車）の保存

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items
-  has_many :orders
+  # has_many :items
+  # has_many :orders
 
   with_options presence: true do
     # 存在すること・確認用を含めて2回入力・6字以上はdeviseのデフォルト実装のため省略
@@ -16,8 +16,8 @@ class User < ApplicationRecord
     validates :nickname
     # @含むこと・存在することはdeviseのデフォルト実装のため省
     # 全角ひらがな、全角カタカナ、漢字
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :last_name, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
+    validates :first_name, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
     # 全角カタカナ
     validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
     validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
