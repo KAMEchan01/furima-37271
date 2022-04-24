c3e523a75c36ab35723b63ef0151295dcbf12fb5
KAMEchan01 <shinichi.kameda001@gmail.com>
2022-04-24 21:55:31 +0900

    ActiveHashモデルファイル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  # アクティブハッシュとのアソシエーション
+  belongs_to :item_category
+  belongs_to :sale_status
+  belongs_to :shipping_fee
+  belongs_to :prefectuer
+  belongs_to :scheduled_delivery
+
 end
