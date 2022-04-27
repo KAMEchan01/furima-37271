5d6ca63f0ee5b858e7d573fe05dfd6cfaae77ca6
KAMEchan01 <shinichi.kameda001@gmail.com>
2022-04-27 20:23:27 +0900

    出品情報をデータベースに保存

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,26 +11,24 @@ class Item < ApplicationRecord
   validates :item_category_id,       presence: true
   validates :sale_status_id,         presence: true
   validates :shipping_fee_id,        presence: true
-  validates :prefectuer_id,          presence: true
-  validates :scheduled__delivery_id, presence: true
+  validates :prefecture_id,          presence: true
+  validates :scheduled_delivery_id,  presence: true
   validates :user_id,                presence: true
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 } , presence: true
   
   
   # アクティブハッシュとのアソシエーション
   belongs_to :item_category
   belongs_to :sale_status
   belongs_to :shipping_fee
-  belongs_to :prefectuer
+  belongs_to :prefecture
   belongs_to :scheduled_delivery
 
   #ジャンルの選択が「--」の時は保存できないようにする
   validates :item_category_id,       numericality: { other_than: 1 , message: "can't be blank"}
   validates :sale_status_id,         numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_fee_id,        numericality: { other_than: 1 , message: "can't be blank"}
-  validates :prefectuer_id,          numericality: { other_than: 1 , message: "can't be blank"}
-  validates :scheduled__delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
-
-
+  validates :prefecture_id,          numericality: { other_than: 1 , message: "can't be blank"}
+  validates :scheduled_delivery_id,  numericality: { other_than: 1 , message: "can't be blank"}
 
 end
