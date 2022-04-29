37042d575415e0431ace5179efafdf1e1de9609a
KAMEchan01 <shinichi.kameda001@gmail.com>
2022-04-29 17:40:19 +0900

    --で登録できない

--- app/models/item.rb
+++ app/models/item.rb
@@ -27,7 +27,7 @@ class Item < ApplicationRecord
   validates :item_category_id,       numericality: { other_than: 1 , message: "can't be blank"}
   validates :sale_status_id,         numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_fee_id,        numericality: { other_than: 1 , message: "can't be blank"}
-  validates :prefecture_id,          numericality: { other_than: 0 , message: "can't be blank"}
+  validates :prefecture_id,          numericality: { other_than: 1 , message: "can't be blank"}
   validates :scheduled_delivery_id,  numericality: { other_than: 1 , message: "can't be blank"}
 
 end
