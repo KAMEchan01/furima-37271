d57069110d33b8496a0b68583fb1b510643ba083
KAMEchan01 <shinichi.kameda001@gmail.com>
2022-04-30 09:05:34 +0900

    active_hashを使用データのテストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -88,7 +88,35 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price is not a number")
       end
 
+      it 'カテゴリーが「---」だと出品できない' do
+        @item.item_category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item category can't be blank")
+      end
 
+      it '商品の状態が「---」だと出品できない' do
+        @item.sale_status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Sale status can't be blank")
+      end
+
+      it '配送料の負担が「---」だと出品できない' do
+        @item.shipping_fee_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee can't be blank")
+      end
+      
+      it '発送元の地域が「---」だと出品できない' do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+
+      it '発送までの日数が「---」だと出品できない' do
+        @item.scheduled_delivery_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Scheduled delivery can't be blank")
+      end
     end
   end
 end
