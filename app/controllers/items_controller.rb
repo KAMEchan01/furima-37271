c1c8f549417122bf144939b98f38b7778a1e4974
KAMEchan01 <shinichi.kameda001@gmail.com>
2022-04-30 21:12:04 +0900

    新しい順に表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
-    @items = Item.all
+    @items = Item.all.order('created_at DESC')
   end
 
   def new
