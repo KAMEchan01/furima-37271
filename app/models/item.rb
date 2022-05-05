fde0bdf2250e968c3f53d4cb143ba96769be9834
KAMEchan01 <shinichi.kameda001@gmail.com>
2022-05-05 19:13:51 +0900

    テーブルアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,9 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+  has_one :order
+  
+
 
   validates :image,                  presence: true
   validates :item_name,              presence: true
