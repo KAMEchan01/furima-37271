dfa0ad1d426059210deacf798267dabd70e4b468
KAMEchan01 <shinichi.kameda001@gmail.com>
2022-05-05 19:00:22 +0900

    address テーブルの修正

--- db/migrate/20220505090649_create_addresses.rb
+++ db/migrate/20220505090649_create_addresses.rb
@@ -6,7 +6,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
       t.integer    :prefecture_id, null: false
       t.string     :city,          null: false
       t.string     :address1,      null: false
-      t.string     :address2,
+      t.string     :address2
       t.string     :phone,         null: false
       t.timestamps
     end
