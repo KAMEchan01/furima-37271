7bbacc11150074164bfc9d973d18fb8ecec5bc9b
KAMEchan01 <shinichi.kameda001@gmail.com>
2022-05-07 16:16:28 +0900

    購入画面の繊維に成功

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,18 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show,:create]
 
-  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
+
 
   def index
     @order_address = OrderAddress.new
   end
 
-  def new
-  end
+
 
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
-      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -23,13 +23,16 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address1, :address2, :phone).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address1, :address2, :phone).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
   def address_params
     params.permit(:postal_code, :prefecture, :city, :address1, :address2, :phone ).merge(doder_id: @order.id)
   end
 
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 
 
 end
