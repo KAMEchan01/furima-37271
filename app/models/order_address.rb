c40be16a6bd72663a80d7e228722a7e3e1b38bf3
KAMEchan01 <shinichi.kameda001@gmail.com>
2022-05-07 16:45:11 +0900

    購入フォーム実装

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address1, :address2, :phone, :token
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address1, :address2, :phone
 
   with_options presence: true do
     validates :user_id
@@ -11,16 +11,14 @@ class OrderAddress
     validates :address1
     validates :address2
     validates :phone, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
-  
-    validates :token
   end
+   
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
    
     def save
       order = Order.create(user_id: user_id, item_id: item_id)
+      
       # 住所を保存する
-      # donation_idには、変数donationのidと指定する
       Address.create(postal_code: postal_code, prefecture: prefecture, city: city, address1: address1, address2: address2, order_id: order.id)
     end
-
 end
