e3655c1a08712209b8b72e689b0d104616be6364
KAMEchan01 <shinichi.kameda001@gmail.com>
2022-05-08 12:33:30 +0900

    カード情報をトークン値非表示

---
+++ app/javascript/card.js
@@ -0,0 +1,29 @@
+const pay = () => {
+  Payjp.setPublicKey("pk_test_50fb5f1757d980a560a3bacc");
+  const submit = document.getElementById("button");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("order_address[number]"),
+      cvc: formData.get("order_address[cvc]"),
+      exp_month: formData.get("order_address[exp_month]"),
+      exp_year: `20${formData.get("order_address[exp_year]")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden">`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+        debugger;
+       }
+    });
+  });
+};
+
+window.addEventListener("load", pay);
